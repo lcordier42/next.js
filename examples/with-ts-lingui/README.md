d6bee46963527c5a980dfd4654d3243f8520c8f5
Ludovic Cordier <ludovic.cordier9@gmail.com>
2017-12-27 15:34:49 +0100

    Clear the repository and add README

---
+++ examples/with-ts-lingui/README.md
@@ -0,0 +1,7 @@
+npm i
+tsc
+touch locale/fr/messages.js
+BABEL_ENV=lingui lingui extract && lingui compile
+BABEL_ENV=node node_modules/.bin/babel pages/index.js -o pages/index.js
+npm run dev
+
--- examples/with-ts-lingui/locale/fr/messages.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    l: {
-        p: function(n, ord) {
-            if (ord) return n == 1 ? "one" : "other";
-            return n >= 0 && n < 2 ? "one" : "other";
-        }
-    },
-    m: { "Hello World.": "Bonjour le monde" }
-};
